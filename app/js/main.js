a6af29f973d26e034f94986c19a662dd4879a15c
svilkov87 <svilkov87@mail.ru>
2017-03-29 01:48:41 +0400

    0.1

---
+++ app/js/main.js
@@ -0,0 +1,127 @@
+$(document).ready(function(){
+  //выпадающий блок "о нас"
+  $("#show_nav").click(function(){
+    $(".ul_menu").slideToggle(100);
+  });
+    //выпадающий доп меню в лк (навигация)
+  $("#showNav").click(function(){
+    $(".lk_ul_child").slideToggle(100);
+  });
+
+  var one = 13;
+
+  //убрать садбар
+    $("#close_sb").click(function(){
+    $(".lk_sidebar").toggleClass('close');
+    $(".lk_wrapp_content").toggleClass('wr_left');
+  });
+
+// выпадающее меню
+  $("#justify_nav").click(function(){
+    $(".menu").fadeToggle(500);
+  });
+
+  //могоуровневое меню
+    $('.ul_menu .li_nav').hover(
+        function() {
+        $(this).css("background", "#8c878c");
+        $('a', this).css("color", "#fff");
+        $('ul:first', this).slideDown(150);
+    },
+        function() {
+          $(this).css("background", "#444050");
+          $('a', this).css("color", "#ffbc00");
+          $('ul:first', this).slideUp(150);
+        }
+    );
+
+    // форма отправки заказа
+    $('.btn_modal').click(function(e){
+        e.preventDefault();
+        var name = $('#name').val(),
+            s_name = $('#s_name').val(),
+            field = $('#modal_field').val();
+
+        if( name == "" || s_name == "" || field == ""){
+            $('.err_block').css("display" , "block");
+        }
+        else {
+            $('.err_block').css("display" , "none");
+            $.ajax({
+                url: "../../ajax/upload.php",
+                type: "POST",
+                data: $('.myform').serialize(),
+                dataType: "html"
+            }).done(function(){
+                // $('#myModlal').css("display" , "none");
+                $('.modal_forms').css("display" , "none");
+                $('.modal_confirm').css("display" , "block");
+                // alert('data');
+            });
+        }
+        $('#name, #s_name, #modal_field').focus(function(){
+            $('.err_block').css("display" , "none");
+        });
+    });
+
+    //Плавный скролл до блока .div по клику на .scroll
+  //Документация: https://github.com/flesler/jquery.scrollTo
+  $("#fa-angle-down").click(function() {
+    $.scrollTo($("#main_about"), 800, {
+      offset: 0
+    });
+  });
+
+    //modal
+    var modal = document.getElementById('myModlal'),
+        btnModal = document.getElementsByClassName('send_need')[0],
+        close = document.getElementsByClassName('close')[0];
+
+    btnModal.onclick = function (event) {
+      event.preventDefault();
+        modal.style.display = "block";
+    }
+    close.onclick = function () {
+        modal.style.display = "none";
+    }
+    //закрытие модал, если юзер кликает на bg
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+  // показать кнопку наверх
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 350){
+      $('#top').fadeIn(100);
+    }
+    else{
+      $('#top').fadeOut(100);
+    }
+  });
+
+  //Кнопка "Наверх"
+  //Документация:
+  //http://api.jquery.com/scrolltop/
+  //http://api.jquery.com/animate/
+  $("#top").click(function () {
+    $("body, html").animate({
+      scrollTop: 0
+    }, 800);
+    return false;
+  });
+
+    //фиксированный нав
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 150){
+            $('.nav_bottom').addClass("fixed_down");
+            $('.nav').css("margin-bottom", "60px");
+        }
+        else{
+            $('.nav_bottom').removeClass("fixed_down");
+            $('.nav').css("margin-bottom", "0");
+        }
+    });
+
+});
---
+++ gulpfile.js
@@ -0,0 +1,175 @@
+'use strict';
+
+var gulp = require('gulp'),// подключение галп
+	sass = require('gulp-sass'),
+    //hash
+    rev_append = require('gulp-rev-append'),
+    rev = require('gulp-rev'),//
+    revCollector = require('gulp-rev-collector'),
+    revOutdated = require('gulp-rev-outdated'),
+    gutil  = require('gulp-util'),
+    rimraf = require('rimraf'),
+    path = require('path'),
+    through = require('through2'),
+	//concatCss = require('gulp-concat-css'),//присоединяем все файлы css в один
+	rename = require('gulp-rename'),//переименовываем конкат и миниф файл
+	cleanCSS = require('gulp-clean-css'),//минифицируем
+	autoprefixer = require('gulp-autoprefixer'),//автопрефикер
+	livereload = require('gulp-livereload'),//
+	connect = require('gulp-connect'),//соед с уд сервером
+	concat = require('gulp-concat'),//конкатенация
+	uglify = require('gulp-uglify'),
+	imagemin = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
+	imageminJpegtran = require('imagemin-jpegtran'),//Compress JPEG images
+	imageminOptipng = require('imagemin-optipng'), //Compress PNG images
+	imageminSvgo = require('imagemin-svgo'),//Compress SVG images
+	pngquant = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
+	cache = require('gulp-cache'),//когда картинко много кэш
+	notify = require("gulp-notify");//уведомление о действии
+
+//livereload
+	gulp.task('connect', function() {
+	  connect.server({
+	    root: 'app',
+	    livereload: true
+	  });
+	});
+
+//hash
+gulp.task('rev_append', function() {
+    gulp.src('app/*.html')
+        .pipe(rev_append())
+        .pipe(gulp.dest('app/'));
+});
+
+gulp.task('rev', function () {
+    return gulp.src('app/sass/**/*.scss')
+        .pipe(sass())
+        .pipe(cleanCSS()) //минифицируем css
+        .pipe(rev())
+        .pipe(gulp.dest('app/css/'))
+        .pipe(rev.manifest())
+        .pipe(gulp.dest('app/manifests/'));
+});
+
+
+gulp.task('rev_collector', ['rev'], function () {
+    return gulp.src(['app/manifests/**/*.json', 'app/*.html'])//
+        .pipe( revCollector({
+            replaceReved: true
+        }) )
+        .pipe( gulp.dest('app/') );
+});
+
+function cleaner() {
+    return through.obj(function(file, enc, cb){
+        rimraf( path.resolve( (file.cwd || process.cwd()), file.path), function (err) {
+            if (err) {
+                this.emit('error', new gutil.PluginError('Cleanup old files', err));
+            }
+            this.push(file);
+            cb();
+        }.bind(this));
+    });
+}
+
+gulp.task('cleanHash', ['rev_collector'], function() {
+    gulp.src( ['app/**/*.*'], {read: false})
+        .pipe( revOutdated(1) ) // leave 1 latest asset file for every file name prefix.
+        .pipe( cleaner() );
+
+    return;
+});
+
+gulp.task('rev_all', ['rev_collector', 'rev', 'cleanHash']);
+
+//sass и css
+// gulp.task('sass', function () {
+gulp.task('sass', ['rev_all'], function () {
+  return gulp.src(['app/sass/**/*.sass', 'app/sass/**/*.scss'])//путь к папке с файлами, м которыми будем работать
+    .pipe(autoprefixer({
+        browsers: ['last 15 versions']
+    }))
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(cleanCSS()) //минифицируем css
+    .pipe(rename('main.min.css'))//как назовем скомпилированный min- файл
+    .pipe(gulp.dest('app/css'))//куда выкладываем итоговый файл
+    .pipe(connect.reload());
+});
+
+//js
+gulp.task('scripts', ['rev_all'], function() {
+  return gulp.src('app/js/main.js')
+    .pipe(concat('all.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js'))
+    .pipe(connect.reload());
+});
+
+//html
+gulp.task('html', function () {
+	gulp.src('app/*.html')
+	.pipe(connect.reload());
+});
+//css
+gulp.task('css', function () {
+	gulp.src('app/libs/jquery.bxslider/*.css')
+	.pipe(connect.reload());
+});
+
+//автом вызов галп при любом изменении следующих файлов
+gulp.task('watch', function () {
+	gulp.watch(['app/sass/**/*.sass', 'app/sass/**/*.scss'], ['sass']) //следим за изменениями всех css, и при их изменении запускаем таск css
+	gulp.watch('app/js/*.js', ['scripts'])
+	gulp.watch('app/libs/jquery.bxslider/*.css', ['css'])
+	gulp.watch('app/*.html', ['html']);
+});
+
+//удаление папки dist перед сборкой
+gulp.task('clean', function() {
+	return del.sync('dist'); // Удаляем папку dist перед сборкой
+});
+
+//чистим кэш
+gulp.task('clear', function (callback) {
+	return cache.clearAll();
+})
+
+//оптимизация изображений
+gulp.task('img', function() {
+	return gulp.src('app/img/**/*') // Берем все изображения из app
+		.pipe(cache(imagemin({  // Сжимаем их с наилучшими настройками с учетом кеширования
+			interlaced: true,
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		})))
+		.pipe(gulp.dest('dist/img')); // Выгружаем на продакшен
+});
+
+//в продакшн
+gulp.task('build', ['clean', 'img', 'sass', 'scripts'], function() {
+	var buildCss = gulp.src(['app/css/style.min.css'])
+	.pipe(gulp.dest('dist/css'));
+
+	var buildFonts = gulp.src('app/libs/fonts/**/*') // Переносим шрифты в продакшен
+	.pipe(gulp.dest('dist/libs/fonts'));
+
+	var buildJs = gulp.src('app/js/**/*') // Переносим скрипты в продакшен
+	.pipe(gulp.dest('dist/js'));
+
+	var buildHtml = gulp.src('app/*.html') // Переносим HTML в продакшен
+	.pipe(gulp.dest('dist'));
+
+});
+
+//задачи по-умолчанию
+gulp.task('default', ['connect', 'watch','sass', 'css', 'html','scripts']);
+
+
+
+
+
+
+
+
